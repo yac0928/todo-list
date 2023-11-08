661a86d78b33e99e2a8936b84b8616b6bd1926b0
yac0928 <kangarooinapark@gmail.com>
2023-11-08 16:05:33 +0800

    router init

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+app.get('/todos', (req, res) => {
+	res.send('get all todos')
+})
+
+app.get('/todos/new', (req, res) => {
+	res.send('create todo')
+})
+
+app.post('/todos', (req, res) => {
+	res.send('add todo')
+})
+
+app.get('/todos/:id', (req, res) => {
+	res.send(`get todo: ${req.params.id}`)
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+	res.send(`get todo edit: ${req.params.id}`)
+})
+
+app.put('/todos/:id', (req, res) => {
+	res.send('modify todo')
+})
+
+app.delete('/todos/:id', (req, res) => {
+	res.send('delete todo')
+})
+
 app.listen(3000, () => {
   console.log('App is running on port 3000')
 })
