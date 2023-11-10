63d43fefde22ba78a85e089b327c1f7480771c67
yac0928 <kangarooinapark@gmail.com>
2023-11-10 16:47:54 +0800

    initialize handlebars layout

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const port = 3000
 const db = require('./models')
 const Todo = db.Todo
 
+const { engine } = require('express-handlebars')
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 app.get('/todos', (req, res) => {
